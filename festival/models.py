63c17f74e08137956ab1b3e8e740498b5e28e49f
poffpoff <baptiste.declerckreseau.eseo.fr>
2017-11-05 14:30:44 +0100

    adding social icons

--- festival/models.py
+++ festival/models.py
@@ -33,31 +33,47 @@ class Card(models.Model):
 class Main(models.Model):
     name = models.CharField(max_length=30)
 
+    color_1 = ColorField(default='#000000')
+    color_2 = ColorField(default='#FFFFFF')
+    color_highlight = ColorField(default='#FFFFFF')
+
+    font_style_navigation_bar = models.CharField( max_length =50, default='Crimson Text')
+    font_size_navigation_bar = models.IntegerField(default='22')
+
+    use_side_bar_image = models.BooleanField(default=False)
+    side_bar_image = models.FileField('File', upload_to='./festival/userImages/')
+
     video_link = models.URLField()
 
     use_image_in_front_of_video = models.BooleanField(default=False)
     image_in_front_of_video = models.FileField('File', upload_to='./festival/userImages/')
 
-    cards = models.ManyToManyField(Card)
-    pages = models.ManyToManyField(Page)
-
-    color_1 = ColorField(default='#000000')
-    color_2 = ColorField(default='#FFFFFF')
-    color_highlight = ColorField(default='#FFFFFF')
-
     use_image_as_background = models.BooleanField(default=False)
     no_repeat_image = models.BooleanField(default=False)
     background_image = models.FileField('File', upload_to='./festival/userImages/')
 
+    pages = models.ManyToManyField(Page)
+    cards = models.ManyToManyField(Card)
 
-    use_side_bar_image = models.BooleanField(default=False)
-    side_bar_image = models.FileField('File', upload_to='./festival/userImages/')
+    show_instagram = models.BooleanField(default=False)
+    https_instagram = models.CharField(max_length=999 , default="#")
 
+    show_facebook = models.BooleanField(default=False)
+    https_facebook = models.CharField(max_length=999, default="#")
 
+    show_twitter = models.BooleanField(default=False)
+    https_twitter = models.CharField(max_length=999, default="#")
 
-    font_style_navigation_bar = models.CharField( max_length =50, default='Crimson Text')
-    font_size_navigation_bar = models.IntegerField(default='22')
+    show_snapchat = models.BooleanField(default=False)
+    https_snapchat = models.CharField(max_length=999, default="#")
 
+    show_youtube = models.BooleanField(default=False)
+    https_youtube = models.CharField(max_length=999, default="#")
+
+    show_contact_us = models.BooleanField(default=False)
+    https_contact_us = models.CharField(max_length=999, default="#")
+
+    use_official_color_for_icons = models.BooleanField(default=False)
 
     def __str__(self):
         return str(self.name)
