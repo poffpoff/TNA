3a20b6003c2a9a5a8af17f871b299f1173f9fbd0
Max <maximilien.tordeux@gmail.com>
2017-12-10 17:55:08 +0100

    update portfolio

--- festival/models.py
+++ festival/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from colorfield.fields import ColorField
 from django.utils import timezone
 
+
 # Create your models here.
 
 class PageType(models.Model):
@@ -10,10 +11,7 @@ class PageType(models.Model):
     def __str__(self):
         return str(self.name)
 
-class Portfolio(PageType):
 
-    def __str__(self):
-        return str(self.name)
 
 
 class Calendar(PageType):
@@ -21,9 +19,10 @@ class Calendar(PageType):
     def __str__(self):
         return str(self.name)
 
+
 class Page(models.Model):
     name = models.CharField(max_length=30)
-    to = models.ForeignKey(PageType, default=0 ,on_delete=models.CASCADE)
+    to = models.ForeignKey(PageType, default=0, on_delete=models.CASCADE)
 
     def __str__(self):
         return str(self.name)
@@ -31,14 +30,21 @@ class Page(models.Model):
 
 # class Folio du portefolio
 class Folio(models.Model):
-    name = models.CharField(max_length=30, default="card")
+    name = models.CharField(max_length=30, default="folio")
     image = models.FileField('File', upload_to='./festival/userImages/')
-    text = models.CharField(max_length=500, default='Text')
+    text = models.TextField(max_length=500, default='Text')
+
+
     # https = models.CharField(max_length=999, default="#")
 
     def __str__(self):
         return str(self.name)
-
+class Portfolio(PageType):
+    folios = models.ManyToManyField(Folio)
+    pageHeading = models.CharField(max_length=30, default="heading")
+    secondarytext = models.TextField(max_length=500, default='subtext')
+    def __str__(self):
+        return str(self.name)
 
 
 class Card(models.Model):
@@ -66,7 +72,7 @@ class Main(models.Model):
     color_2 = ColorField(default='#FFFFFF')
     color_highlight = ColorField(default='#FFFFFF')
 
-    font_style_navigation_bar = models.CharField( max_length =50, default='Crimson Text')
+    font_style_navigation_bar = models.CharField(max_length=50, default='Crimson Text')
     font_size_navigation_bar = models.IntegerField(default='22')
 
     use_side_bar_image = models.BooleanField(default=False)
@@ -85,7 +91,7 @@ class Main(models.Model):
     cards = models.ManyToManyField(Card)
 
     show_instagram = models.BooleanField(default=False)
-    https_instagram = models.CharField(max_length=999 , default="#")
+    https_instagram = models.CharField(max_length=999, default="#")
 
     show_facebook = models.BooleanField(default=False)
     https_facebook = models.CharField(max_length=999, default="#")
@@ -107,15 +113,11 @@ class Main(models.Model):
     def __str__(self):
         return str(self.name)
 
-
     def fileLink(self):
         if self.File:
             return '<a href="' + str(self.File.url) + '">' + 'NameOfFileGoesHere' + '</a>'
         else:
             return '<a href="''"></a>'
 
-
     fileLink.allow_tags = True
     fileLink.short_description = "File Link"
-
-
