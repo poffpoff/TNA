8aae8583503487a17788ad08cdf9978eec02ca8c
poffpoff <baptiste.declerckreseau.eseo.fr>
2017-12-12 22:36:43 +0100

    use of TabularInline

--- festival/admin.py
+++ festival/admin.py
@@ -3,10 +3,20 @@ from django.contrib import admin
 # Register your models here.
 from .models import Main, Page, Card, Calendar, Portfolio, Folio
 
+class FoliosInline(admin.TabularInline):
+    model = Portfolio.folios.through
+
+
+class PortfolioAdmin(admin.ModelAdmin):
+    inlines = [
+        FoliosInline,
+    ]
+    exclude = ('folios',)
+
 admin.site.register(Main)
 admin.site.register(Page)
 admin.site.register(Card)
 admin.site.register(Calendar)
-admin.site.register(Portfolio)
+admin.site.register(Portfolio, PortfolioAdmin)
 admin.site.register(Folio)
 
